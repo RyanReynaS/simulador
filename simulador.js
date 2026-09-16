e6e7713ccbab7b23374c5526c97225ff9e354277
RyanReynaS <ryanedurs17@gmail.com>
2026-09-16 09:57:48 -0500

    lectura de ingresos y egresos en calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,7 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    // Paso 3: Leer ingresos, egresos y calcular disponible
+    let ingresos = recuperarFloat("txtIngresos");
+    let egresos = recuperarFloat("txtEgresos");
+    let disponible = calcularDisponible(ingresos, egresos);
+    mostrarTexto("lblDisponibleValor", disponible.toFixed(2));
+}
