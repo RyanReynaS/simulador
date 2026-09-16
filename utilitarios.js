fe70f2ba528bc68303ac808939aeb7ec1e5f4901
RyanReynaS <ryanedurs17@gmail.com>
2026-09-16 10:06:43 -0500

    agregando utilitarios

---
+++ utilitarios.js
@@ -0,0 +1,25 @@
+function recuperarFloat(idComponente) {
+    let valor = document.getElementById(idComponente).value;
+    return parseFloat(valor) || 0;
+}
+
+function recuperarEntero(idComponente) {
+    let valor = document.getElementById(idComponente).value;
+    return parseInt(valor) || 0;
+}
+
+function mostrarTexto(idComponente, mensaje) {
+    document.getElementById(idComponente).innerText = mensaje;
+}function recuperarFloat(idComponente) {
+    let valor = document.getElementById(idComponente).value;
+    return parseFloat(valor) || 0;
+}
+
+function recuperarEntero(idComponente) {
+    let valor = document.getElementById(idComponente).value;
+    return parseInt(valor) || 0;
+}
+
+function mostrarTexto(idComponente, mensaje) {
+    document.getElementById(idComponente).innerText = mensaje;
+}
